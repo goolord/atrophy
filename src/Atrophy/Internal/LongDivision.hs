6aefdc4db294a4f8ebe50371da46be43a8a561f4
Zachary Churchill <zacharyachurchill@gmail.com>
2022-03-06 19:28:13 -0500

    fix some comments

--- src/Atrophy/Internal/LongDivision.hs
+++ src/Atrophy/Internal/LongDivision.hs
@@ -20,7 +20,7 @@ import Data.Foldable (for_)
 import Data.Functor ((<&>))
 
 -- divides a 128-bit number by a 64-bit divisor, returning the quotient as a 64-bit number
--- assumes that the divisor and numerator have both already been bit-shifted so that divisor.leading_zeros() == 0
+-- assumes that the divisor and numerator have both already been bit-shifted so that countLeadingZeros divisor == 0
 {-# INLINE divide128By64Preshifted #-}
 divide128By64Preshifted :: Word64 -> Word64 -> Word64 -> Word64
 divide128By64Preshifted numeratorHi numeratorLo' divisor = runST $ do
@@ -30,15 +30,15 @@ divide128By64Preshifted numeratorHi numeratorLo' divisor = runST $ do
     divisorFull128 = fromIntegral @Word64 @Word128 divisor
     divisorHi = divisor `shiftR` 32
 
-    -- To get the upper 32 bits of the quotient, we want to divide 'full_upper_numerator' by 'divisor'
-    -- but the problem is, full_upper_numerator is a 96-bit number, meaning we would need to use u128 to do the division all at once, and the whole point of this is that we don't want to do 128 bit divison because it's slow
+    -- To get the upper 32 bits of the quotient, we want to divide 'fullUpperNumerator' by 'divisor'
+    -- but the problem is, fullUpperNumerator is a 96-bit number, meaning we would need to use u128 to do the division all at once, and the whole point of this is that we don't want to do 128 bit divison because it's slow
     -- so instead, we'll shift both the numerator and divisor right by 32, giving us a 64 bit / 32 bit division. This won't give us the exact quotient -- but it will be close.
     fullUpperNumerator = (Word128 0 numeratorHi `shiftL` 32) .|. numeratorMid
     quotientHi' = min (numeratorHi `div` divisorHi) (fromIntegral $ maxBound @Word32)
   quotientHi <- newSTRef quotientHi'
   productHi <- newSTRef $ (Word128 0 quotientHi') * divisorFull128
 
-  -- quotient_hi contains our guess at what the quotient is! the problem is that we got this by ignoring the lower 32 bits of the divisor. when we account for that, the quotient might be slightly lower
+  -- quotientHi contains our guess at what the quotient is! the problem is that we got this by ignoring the lower 32 bits of the divisor. when we account for that, the quotient might be slightly lower
   -- we will know our quotient is too high if quotient * divisor > numerator. if it is, decrement until it's in range
   whileM_ ((>) <$> readSTRef productHi <*> pure fullUpperNumerator) $ do
     modifySTRef quotientHi (\x -> x - 1)
@@ -54,7 +54,7 @@ divide128By64Preshifted numeratorHi numeratorLo' divisor = runST $ do
     x <- readSTRef quotientLo
     newSTRef $ (Word128 0 x) * divisorFull128
 
-  -- again, quotient_lo is just a guess at this point, it might be slightly too large
+  -- again, quotientLo is just a guess at this point, it might be slightly too large
   whileM_ ((>) <$> readSTRef productLo <*> pure fullLowerNumerator) $ do
     modifySTRef quotientLo (\x -> x - 1)
     modifySTRef productLo (\x -> x - divisorFull128)
